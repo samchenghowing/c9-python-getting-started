eab64f7a54d83c15c02e1e9ebb2fb67f12808b31
Ho wing Cheng <howingcheng@Hode-MacBook-Air.local>
2020-09-21 14:31:30 +0800

    commit before vue

---
+++ even-more-python-for-beginners-data-tools/01 - Jupyter Notebooks/testingUDP.py
@@ -0,0 +1,21 @@
+import json
+
+class Person:
+  def __init__(self, name, age):
+    self.name = name
+    self.age = age
+
+p1 = Person("John", 36)
+
+print(p1.name)
+print(p1.age)
+
+# some JSON:
+x =  '{ "name":"John", "age":30, "city":"New York"}'
+
+# parse x:
+y = json.loads(x)
+
+# the result is a Python dictionary:
+print(y["age"])
+#this.// 
