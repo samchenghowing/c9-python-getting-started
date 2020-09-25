1ecb5fb72bae96a87183843906e1ee6beb6ccaba
samchenghowing <56993697+samchenghowing@users.noreply.github.com>
2020-09-25 16:32:25 +0800

    Update main.js
    fall back

--- new-project/src/main.js
+++ new-project/src/main.js
@@ -1,6 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+const router = new VueRouter()
+
+router.beforeEach((to, from, next) => {
+  // ...
+})
+
 new Vue({
   el: '#app',
   render: h => h(App)
